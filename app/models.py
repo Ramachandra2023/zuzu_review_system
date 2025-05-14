8e9aace21c11c39a350d4a6982552d0a117a9394
Ramachandra2023 <153800754+Ramachandra2023@users.noreply.github.com>
2025-05-14 20:45:19 +0530

    Update models.py
    Added Processed files to achieve idempotent

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, JSON
+from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -14,3 +15,9 @@ class Review(Base):
     reviewer_info = Column(JSON)
     review_text = Column(String)
     metadata = Column(JSON)
+
+class ProcessedFile(Base):
+    __tablename__ = "processed_files"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String, unique=True, nullable=False)
+    processed_at = Column(DateTime, default=datetime.utcnow)
